c2391b574ead9d7480494f9e4f8818e9967d06f5
SriviB <srivibalaji33@gmail.com>
2018-07-25 15:55:13 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -28,6 +28,13 @@ end
 
 puts change_rainbow_colors
 def add_colors
+  rainbow_colors = [
+  "red",
+  "light_red",
+  "light_yellow"
+]
   rainbow_colors.push("green", "blue")
   rainbow_colors
 end
+
+puts add_colors
